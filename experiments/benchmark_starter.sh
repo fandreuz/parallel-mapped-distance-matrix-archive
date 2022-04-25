409315f8a20a88064614ae39bca11b02ff3ad786
fAndreuzzi <fandreuz@abel.maths.sissa.it>
2022-04-25 10:13:32 +0200

    add Future experiments

--- experiments/benchmark_starter.sh
+++ experiments/benchmark_starter.sh
@@ -5,3 +5,7 @@ done
 for file in $(ls pmb*.py); do
 echo "$file : $(python3 $file $1 $2)" >> "output${1}_${2}.txt";
 done
+
+for file in $(ls pf*.py); do
+echo "$file : $(python3 $file $1 $2)" >> "output${1}_${2}.txt";
+done
