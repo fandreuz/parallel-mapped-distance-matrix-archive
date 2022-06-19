8c16a228a81639188b25c9092a439b0c255dfbf5
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2022-06-19 22:45:25 +0200

    bugfix

--- parallel_inverse_aggregate.py
+++ parallel_inverse_aggregate.py
@@ -2,6 +2,67 @@ import numpy as np
 
 from dask.distributed import as_completed
 
+# convenient way to extract slices from a NumPy array. the method used when
+# the number of dimensions is greater than 3 is not efficient, and thus is used
+# only when writing the expression by hand becomes too tedious
+def extract_slices(arr, lower_bounds, upper_bounds):
+    if len(lower_bounds) == 1:
+        return arr[lower_bounds[0] : upper_bounds[0]]
+    elif len(lower_bounds) == 2:
+        return arr[
+            lower_bounds[0] : upper_bounds[0],
+            lower_bounds[1] : upper_bounds[1],
+        ]
+    elif len(lower_bounds) == 3:
+        return arr[
+            lower_bounds[0] : upper_bounds[0],
+            lower_bounds[1] : upper_bounds[1],
+            lower_bounds[2] : upper_bounds[2],
+        ]
+    else:
+        return arr[
+            tuple(slice(l, u) for l, u in zip(lower_bounds, upper_bounds))
+        ]
+
+
+def sum_slices(arr, val, lower_bounds, upper_bounds):
+    if len(lower_bounds) == 1:
+        arr[lower_bounds[0] : upper_bounds[0]] += val
+    elif len(lower_bounds) == 2:
+        arr[
+            lower_bounds[0] : upper_bounds[0],
+            lower_bounds[1] : upper_bounds[1],
+        ] += val
+    elif len(lower_bounds) == 3:
+        arr[
+            lower_bounds[0] : upper_bounds[0],
+            lower_bounds[1] : upper_bounds[1],
+            lower_bounds[2] : upper_bounds[2],
+        ] += val
+    else:
+        arr[
+            tuple(slice(l, u) for l, u in zip(lower_bounds, upper_bounds))
+        ] += val
+
+
+def periodic_sum(arr, val, lower_bounds, upper_bounds):
+    negative = np.where(lower_bounds < 0, np.abs(lower_bounds), 0)
+    arr_size = np.array(arr.shape)
+    overflow = np.where(
+        upper_bounds > arr_size - 1,
+        np.array(val.shape) - (upper_bounds - arr_size + 1),
+        None,
+    )
+    # TODO improve performance
+    val = val[
+        tuple(slice(bottom, top) for bottom, top in zip(negative, overflow))
+    ]
+
+    np.clip(lower_bounds, 0, None, out=lower_bounds)
+    np.clip(upper_bounds, 0, arr_size - 1, out=upper_bounds)
+
+    sum_slices(arr, val, lower_bounds, upper_bounds)
+
 
 def group_by(a):
     r"""
@@ -80,7 +141,7 @@ def compute_linearized_bin_coords(bins_per_axis, bins_coords):
     shifted_nbins_per_axis = np.ones_like(bins_per_axis, dtype=int)
     shifted_nbins_per_axis[:-1] = bins_per_axis[1:]
 
-    return np.dot(bin_coords, shifted_nbins_per_axis[:, None])
+    return np.dot(bins_coords, shifted_nbins_per_axis[:, None])
 
 
 def extract_subproblems(indexes, n_per_subgroup):
@@ -119,8 +180,8 @@ def extract_subproblems(indexes, n_per_subgroup):
 
 
 def distribute_subproblems(
-    uniform_grid_cell_size,
-    uniform_grid_cell_count,
+    uniform_grid_cell_step,
+    uniform_grid_size,
     bins_size,
     pts,
     weights,
@@ -136,14 +197,12 @@ def distribute_subproblems(
 
     Parameters
     ----------
-    uniform_grid_cell_size: np.ndarray
-        Dimension of a cell of the grid (for each axis). Expected a 1D NumPy
-        array whose length is the number of dimensions of the space.
-    uniform_grid_cell_count: np.ndarray
+    uniform_grid_cell_step: np.ndarray
+    uniform_grid_size: np.ndarray
         Number of cells in the uniform grid (for each axis). Expected a 1D NumPy
         array whose length is the number of dimensions of the space.
     bins_size: np.ndarray
-        Number of cells of the grid in a bin (for each axis). Expected a NumPy
+        Number of cells in a bin (for each axis). Expected a NumPy
         1D array whose length is the number of dimensions of the space.
     pts: np.ndarray
         Non-uniform points scattered in the uniform grid. Expected a 2D NumPy
@@ -170,15 +229,12 @@ def distribute_subproblems(
         4. Weights for the non uniform points in this subproblem.
     """
     # number of bins per axis
-    bins_per_axis = uniform_grid_cell_count // bins_size
+    bins_per_axis = uniform_grid_size // bins_size
 
     # for each point in pts, compute the coordinate in the uniform grid
     bin_coords = np.floor_divide(
-        pts, uniform_grid_cell_size * bins_size
+        pts, uniform_grid_cell_step * bins_size
     ).astype(int)
-    # moves to the last bin of the axis any point which is outside the region
-    # defined by samples2.
-    np.clip(bin_coords, None, uniform_grid_cell_count - 1, out=bin_coords)
 
     # we transform our N-Dimensional bins indexing (N is the number of axes)
     # into a linear one (only one index)
@@ -214,25 +270,24 @@ def distribute_subproblems(
 
     return client.map(
         bin_content_tuple,
-        (subgroup for bin_content in subproblems for subgroup in bin_content),
+        tuple(
+            subgroup for bin_content in subproblems for subgroup in bin_content
+        ),
         pts=pts_fu,
         bin_coords=bin_coords_fu,
     )
 
 
-def build_uniform_grid_bin():
-    return grid, idxes
-
-
 def compute_mapped_distance_on_subgroup(
     subgroup_info,
-    uniform_grid_cell_size,
-    uniform_grid_cell_count,
+    uniform_grid_cell_step,
+    uniform_grid_size,
     bins_size,
-    max_distance_in_cells,
+    max_distance,
     function,
     exact_max_distance,
     reference_bin,
+    dtype,
 ):
     r"""
     Function to be executed on the worker, provides a mapping for each
@@ -243,28 +298,81 @@ def compute_mapped_distance_on_subgroup(
     `tuple`
     """
     # unwrap the content of the future
-    subgroup, bin_coords, nup_idxes, weights = subgroups_and_coords
+    subgroup, bin_coords, nup_idxes, weights = subgroup_info
 
-    uniform_grid, samples1_idxes = build_uniform_grid_bin()
+    # location of the lower left point of the non-padded bin in terms
+    # of uniform grid cells
+    bin_virtual_lower_left = bin_coords * bins_size
+    # location of the upper right point of the non-padded bin
+    bin_virtual_upper_right = bin_virtual_lower_left + bins_size - 1
 
+    # translate the subgroup in order to locate it nearby the reference bin
+    # (reminder: the lower left point of the (non-padded) reference bin is
+    # [0,0]).
+    subgroup -= bin_virtual_lower_left * uniform_grid_cell_step
+
+    # TODO 3d?
     distances = np.linalg.norm(
-        uniform_grid[..., None] - subgroup.T[:, None, None],
-        axis=0,
+        reference_bin[:, :, None] - subgroup[None, None],
+        axis=-1,
     )
 
     if exact_max_distance:
         nearby = distances < max_distance
-        mapped_distance = np.zeros_like(distances, dtype=subgroup.dtype)
+        mapped_distance = np.zeros_like(distances, dtype=dtype)
         mapped_distance[nearby] = function(distances[nearby])
     else:
         mapped_distance = function(distances)
 
-    return mapped_distance, (*samples1_idxes, nup_idxes)
+    # aggregate contributions
+    aggregated_mapped_distance = np.sum(mapped_distance, axis=-1)
+
+    # compute appropriate upper/lower indexing
+    max_distance_in_cells = np.ceil(
+        max_distance / uniform_grid_cell_step
+    ).astype(int)
+    lb = bin_virtual_lower_left - max_distance_in_cells
+    # we add one because the upper bound is not included
+    ub = (bin_virtual_upper_right + 1) + max_distance_in_cells
+
+    assert tuple(ub - lb) == aggregated_mapped_distance.shape
+
+    return aggregated_mapped_distance, lb, ub
+
+
+def generate_uniform_grid(grid_step, grid_size, lower_left=None):
+    r"""
+    Generate an uniform grid according to the given features in `D` dimensions.
+
+    Parameters
+    ----------
+    grid_step: np.ndarray
+        Size of the step of the grid in each direction. Expected a 1D NumPy
+        array whose size is `D`.
+    uniform_grid_size: np.ndarray
+        Number of cells in the grid in each direction. Expected a 1D NumPy
+        array whose size is `D`.
+
+    Returns
+    -------
+    `np.ndarray`
+    """
+    # the tranpose is needed because we want the components in the rightmost
+    # part of the shape
+    integer_grid = extract_slices(
+        np.mgrid, np.zeros_like(grid_size), grid_size
+    ).T
+    # TODO 3d
+    grid = integer_grid * grid_step[None, None]
+
+    if lower_left is not None:
+        grid += lower_left
+    return np.swapaxes(grid, 0, 1)
 
 
 def mapped_distance_matrix(
-    uniform_grid_cell_size,
-    uniform_grid_cell_count,
+    uniform_grid_cell_step,
+    uniform_grid_size,
     bins_size,
     non_uniform_points,
     max_distance,
@@ -273,22 +381,21 @@ def mapped_distance_matrix(
     weights=None,
     exact_max_distance=True,
     pts_per_future=5,
+    dtype=None,
 ):
     r"""
     Compute the mapped distance matrix of a set of non uniform points
     distributed inside a uniform grid whose features are specified in
-    `uniform_grid_cell_size`, `uniform_grid_cell_count`, `bins_size`.
+    `uniform_grid_cell_step`, `uniform_grid_size`, `bins_size`.
 
     Parameters
     ----------
-    uniform_grid_cell_size: np.ndarray
-        Dimension of a cell of the grid (for each axis). Expected a 1D NumPy
-        array whose length is the number of dimensions of the space.
-    uniform_grid_cell_count: np.ndarray
+    uniform_grid_cell_step: np.ndarray
+    uniform_grid_size: np.ndarray
         Number of cells in the uniform grid (for each axis). Expected a 1D NumPy
         array whose length is the number of dimensions of the space.
     bins_size: np.ndarray
-        Number of cells of the grid in a bin (for each axis). Expected a NumPy
+        Number of cells in a bin (for each axis). Expected a NumPy
         1D array whose length is the number of dimensions of the space.
     non_uniform_points: np.ndarray
         Non-uniform points scattered in the uniform grid. Expected a 2D NumPy
@@ -296,7 +403,7 @@ def mapped_distance_matrix(
         columns is the number of dimensions of the space.
     max_distance: int
         Maximum distance between a pair uniform/non-uniform point to be
-        considered not zero, in terms of cells of the uniform grid.
+        considered not zero.
     function: function
         Function used to map the distance between uniform and non-uniform
         points.
@@ -319,36 +426,62 @@ def mapped_distance_matrix(
     """
     if weights is None:
         weights = np.ones(len(non_uniform_points), dtype=int)
+    if dtype is None:
+        dtype = non_uniform_points.dtype
+
+    assert (
+        bins_size.shape
+        == uniform_grid_size.shape
+        == uniform_grid_cell_step.shape
+    )
 
     # split and distribute subproblems to the workers
     subgroups_coords_fu = distribute_subproblems(
-        uniform_grid_cell_size,
-        uniform_grid_cell_count,
-        samples2,
-        bins_size,
+        uniform_grid_cell_step=uniform_grid_cell_step,
+        uniform_grid_size=uniform_grid_size,
+        bins_size=bins_size,
+        pts=non_uniform_points,
         pts_per_future=pts_per_future,
         client=client,
+        weights=weights,
     )
 
+    max_distance_in_cells = np.ceil(
+        max_distance / uniform_grid_cell_step
+    ).astype(int)
+    # build a reference padded bin. the padding is given by taking twice
+    # (in each direction) the value of max_distance. the lower_left point is
+    # set to max_distance because we want the (0,0) point to be the first
+    # point inside the non-padded bin.
+    reference_bin = generate_uniform_grid(
+        uniform_grid_cell_step,
+        bins_size + 2 * max_distance_in_cells,
+        lower_left=-(max_distance_in_cells * uniform_grid_cell_step),
+    )
+
+    # start computation of the mapped distance
     mapped_distances_fu = client.map(
         compute_mapped_distance_on_subgroup,
         subgroups_coords_fu,
-        uniform_grid_cell_count=uniform_grid_cell_count,
-        uniform_grid_cell_size=uniform_grid_cell_size,
+        uniform_grid_size=uniform_grid_size,
+        uniform_grid_cell_step=uniform_grid_cell_step,
         bins_size=bins_size,
         function=func,
-        weights=weights,
+        reference_bin=reference_bin,
         max_distance=max_distance,
         exact_max_distance=exact_max_distance,
+        dtype=dtype,
     )
 
     mapped_distance = np.zeros(
-        (*uniform_grid_cell_count, len(samples2)),
-        dtype=samples2.dtype,
+        uniform_grid_size,
+        dtype=dtype,
     )
-    for _, (submatrix, idxes_broadcastable_tuple) in as_completed(
+    # uniform_grid_md is the mapped distance contribution from the current
+    # subproblem in the indexes uniform_grid_idxes
+    for _, (uniform_grid_md, bin_lb, bin_ub) in as_completed(
         mapped_distances_fu, with_results=True
     ):
-        mapped_distance[idxes_broadcastable_tuple] = submatrix
+        periodic_sum(mapped_distance, uniform_grid_md, bin_lb, bin_ub)
 
-    return mapped_distance.reshape(-1, len(samples2))
+    return mapped_distance
