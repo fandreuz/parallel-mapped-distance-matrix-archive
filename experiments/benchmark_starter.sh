0c2f41a43145ee5153a1f90310c6bd5470a2b03c
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2022-08-15 23:56:01 +0200

    reduce data size

--- experiments/benchmark_starter.sh
+++ experiments/benchmark_starter.sh
@@ -2,7 +2,7 @@ for problem_dim in {1..5}; do
     for file in $(ls runners/); do
         if [ "${file: -3}" == ".py" ]
         then
-            python3 runner.py runners.${file%???} 20 data.random_points 0.1 $(($problem_dim * 4))
+            python3 runner.py runners.${file%???} 20 data.random_points 0.1 $problem_dim
         fi
     done
     echo "Batch $problem_dim finished."
